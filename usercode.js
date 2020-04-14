5ef605eb1d6846ef12064630004528fdc7c126da
Aleksandr <39279984+Oleksenko@users.noreply.github.com>
2020-04-14 15:13:40 +0300

    Create usercode.js

---
+++ usercode.js
@@ -0,0 +1,8 @@
+const moment = require('moment');
+const axios = require('axios');
+
+module.exports = (data) => {
+    data.now = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    return data;
+};
