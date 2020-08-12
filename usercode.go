5bfad76a99007153775b03509b4bb9362f50b87f
Aleksandr <39279984+Oleksenko@users.noreply.github.com>
2020-08-12 17:06:35 +0300

    Update usercode.go

--- usercode.go
+++ usercode.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"context"
-        "os"
+	"os"
+
+	"io/ioutil"
+
+	"fmt"
 
 	"github.com/corezoid/gitcall-go-runner/gitcall"
 )
 
 func usercode(_ context.Context, data map[string]interface{}) error {
-       os.Exit(1)
+	n, ok := data["name"]
+	if !ok {
+		return fmt.Errorf("add unique name to the data")
+	}
+
+	f := fmt.Sprintf("trigger-%s.txt", n)
+	if fileExists(f) {
+		return nil
+	}
+
+	if err := ioutil.WriteFile(f, []byte("1"), 0644); err != nil {
+		return err
+	}
+
+	os.Exit(1)
 
 	return nil
 }
 
 func main() {
 	gitcall.Handle(usercode)
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
