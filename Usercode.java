26cad2aaa95a72fcb0ade42e3c77493c4a063a3a
Aleksandr <39279984+Oleksenko@users.noreply.github.com>
2021-02-09 09:53:51 +0200

    Update Usercode.java

--- Usercode.java
+++ Usercode.java
@@ -1,13 +1,13 @@
 // fq-class name com.corezoid.usercode.Usercode is mandatory
-package com.corezoid.test;
+package test.corezoid.folder;
 
-import com.corezoid.gitcall.runner.api.IUsercode;
+import com.corezoid.gitcall.runner.api.UsercodeHandler;
 import java.util.Map;
 
-public class Test implements IUsercode<Map<String, String>, Map<String, String>> {
+public class Usercode implements UsercodeHandler<Map<String, String>, Map<String, String>> {
     @java.lang.Override
     public Map<String, String> handle(Map<String, String> data) throws Exception {
-        data.put("hello", "Hello java!");
+        data.put("hello", "Test java!");
         return data;
     }
 }
